5cb3d05647a28afafbbabb35d84c1b46ae2ccf4b
AngelBarreraC <420087896@pcpuma.acatlan.unam.mx>
2022-05-28 22:32:10 -0500

    images

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Appis from './Appis';
 function App() {
   return (
     <div className="App">
+      <header className='App-header'>
       <BrowserRouter>
           <Switch>
             <Route path="/" component={Appis} exact /> 
           </Switch>
         </BrowserRouter>
+      </header>
     </div>
   );
 }
--- src/Appis.js
+++ src/Appis.js
@@ -31,23 +31,41 @@ function Appis (){
             console.log(error);  
         }
     }
-    const getVist =async()=>{
+    const[image,setImage]=useState([]);
+    const[vis1,setVist1]=useState(1)
+    const getImage = async () =>{
         try{
-           
+            let formData = new FormData();
+            const url = "https://jsonplaceholder.typicode.com/photos";
 
-        }catch(error){
-            console.log(error)
+            let result = await axios({
+                url,
+                method: 'GET',
+                dataType: 'json',
+                ContentType: 'application/json',
+                data: formData
+            });
+            setImage(result.data)
+            console.log(image.data)
+            setVist1(parseInt(vis1)+1)
+            if(vis1%2 == 0){
+                document.getElementById("vision2").style.display='none';
+            }else{
+                document.getElementById("vision2").style.display='block';
+            }
+        }catch (error) {
+            console.log(error);  
         }
     }
     return(
         <div className="content">
             <form>
-                <div class="row">
-                    <div class="col-6">
+                <div className="row">
+                    <div className="col-6">
                         <button type="button" class="btn btn-outline-primary" id="1" onClick={(getData)}>primary</button>
                     </div>
-                    <div class="col-6" >
-                        <button type="button" class="btn btn-outline-primary" id="2" onClick>Primary</button>
+                    <div className="col-6" >
+                        <button type="button" className="btn btn-outline-primary" id="2" onClick={(getImage)}>Primary</button>
                     </div>
                 </div>
             </form>
@@ -74,7 +92,15 @@ function Appis (){
                 }
                 </tbody>
             </table>
-            <p id="vision2">Tabla</p> 
+            <div class="row" id="vision2 im">{
+                image.map( (data) =>
+                    <div class="col-md-3">
+                    <img src="{image.data.url}" alt="..." class="img-thumbnail"></img>
+                </div>
+                )
+            }
+            
+            </div> 
         </div>
     );
 }
